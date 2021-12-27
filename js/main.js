6759b8f48ffd6eaa1803a47ced173d50ac8cb436
kevinbdx35 <k.bardoux@orange.fr>
2021-12-27 11:39:36 +0100

    add about

--- js/main.js
+++ js/main.js
@@ -33,39 +33,3 @@ const themeSwitcher = {
 
 themeSwitcher.init();
 
-/************************************************************************/
-/*Modal implementation*/
-/************************************************************************/
-
-const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
-const btnClosModal = document.querySelector(".close-modal");
-const btnOpenModal = document.querySelector(".show-modal");
-
-console.log(btnOpenModal);
-
-//Modal opening
-for (let i = 0; i < btnOpenModal.length; i++) {
-  btnOpenModal[i].addEventListener("click", function () {
-    console.log("Button Clicked");
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
-  });
-}
-
-//Modal closing
-const closeModal = function () {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
-};
-
-btnClosModal.addEventListener("click", closeModal);
-overlay.addEventListener("click", closeModal);
-
-//Modal closing with keyboard
-document.addEventListener("keydown", function (e) {
-  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
-    console.log("ok");
-    closeModal();
-  }
-});
