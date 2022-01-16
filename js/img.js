3eddd26db3dc4e16ba280908297075a9052a4b0f
kevinbdx35 <k.bardoux@orange.fr>
2022-01-17 00:29:48 +0100

    add new 3d projects

--- js/img.js
+++ js/img.js
@@ -7,6 +7,6 @@ const imgList = document.querySelectorAll("img");
 for (let i = 0; i < imgList.length; i++) {
   imgList[i].onclick = function () {
     console.log(this.src);
-    window.open(this.src, "myImage", "resize = yes, width = 500, height = 500");
+    window.open(this.src, "myImage", "resize = yes, width = 1000, height = 1000");
   };
 }
